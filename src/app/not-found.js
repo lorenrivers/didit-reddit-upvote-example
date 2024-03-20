ed795f24363b830fb848f282b2a42edbf07b9e1c
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-03-20 11:07:48 +0000

    error page added

---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Cannot vote without being logged in!</h2>
+      <p>Please log in to vote</p>
+    </div>
+  );
+}
